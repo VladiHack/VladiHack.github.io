e80cac1a503ceae45df1e413bc1e12e32bebcc8a
Vladimir Boychev <vlad.boychev05@gmail.com>
2024-07-20 14:59:54 +0300

    Added Website

---
+++ Portfolio/wp-content/uploads/uag-plugin/assets/1000/uag-js-1203.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-332364cc', {"layout":"bars","heading":"C#","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":85,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-10a0bec4', {"layout":"bars","heading":"HTML","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":13,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-97a5b8ce', {"layout":"bars","heading":"CSS","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":2,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-06b617b6', {"layout":"bars","heading":"JavaScript","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":1,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+ });
